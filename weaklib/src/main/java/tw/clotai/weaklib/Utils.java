3c654e5d4804597d16fde2b989f47bd43e625b04
vincent <vincent@opnet.com.tw>
2013-09-07 08:51:34 +0800

    update

--- weaklib/src/main/java/tw/clotai/weaklib/Utils.java
+++ weaklib/src/main/java/tw/clotai/weaklib/Utils.java
@@ -24,6 +24,7 @@ import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.io.Writer;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -735,4 +736,98 @@ public class Utils {
 
         return true;
     }
+
+
+    public static String escapeJavascriptStyle(String str) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            StringWriter writer = new StringWriter(str.length() * 2);
+            escapeJavaStyleString(writer, str, true);
+			writer.flush();
+            return writer.toString();
+        } catch (IOException ioe) {
+            return null;
+        }
+    }
+
+    private static void escapeJavaStyleString(Writer out, String str, boolean escapeSingleQuote) throws IOException {
+        if (out == null) {
+            throw new IllegalArgumentException("The Writer must not be null");
+        }
+
+        if (str == null) {
+            return;
+        }
+        int sz;
+        sz = str.length();
+        for (int i = 0; i < sz; i++) {
+            char ch = str.charAt(i);
+
+            // handle unicode
+            if (ch > 0xfff) {
+                out.write("%25u" + hex(ch));
+            } else if (ch > 0xff) {
+                out.write("%25u0" + hex(ch));
+            } else if (ch > 0x7f) {
+                out.write("%25u00" + hex(ch));
+            } else if (ch < 32) {
+                switch (ch) {
+                    case '\b':
+                        out.write('\\');
+                        out.write('b');
+                        break;
+                    case '\n':
+                        out.write('\\');
+                        out.write('n');
+                        break;
+                    case '\t':
+                        out.write('\\');
+                        out.write('t');
+                        break;
+                    case '\f':
+                        out.write('\\');
+                        out.write('f');
+                        break;
+                    case '\r':
+                        out.write('\\');
+                        out.write('r');
+                        break;
+                    default :
+                        if (ch > 0xf) {
+                            out.write("%25u00" + hex(ch));
+                        } else {
+                            out.write("%25u000" + hex(ch));
+                        }
+                        break;
+                }
+            } else {
+                switch (ch) {
+                    case '\'':
+                        if (escapeSingleQuote) {
+                          out.write('\\');
+                        }
+                        out.write('\'');
+                        break;
+                    case '"':
+                        out.write('\\');
+                        out.write('"');
+                        break;
+                    case '\\':
+                        out.write('\\');
+                        out.write('\\');
+                        break;
+                    default :
+                        out.write(ch);
+                        break;
+                }
+            }
+        }
+    }
+
+    private static String hex(char ch) {
+        return Integer.toHexString(ch).toUpperCase();
+    }
+
 }
